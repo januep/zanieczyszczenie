0e6825a2ae660f8861c598aae3495c3794e307c2
83691 <83691@student.ue.poznan.pl>
2023-12-12 17:39:22 +0100

    data frames added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Witajcie w moim pierwszym projekcie w Pythonie!
 
 import requests
+import pandas as pd
 
 # URL API do pobrania listy stacji pomiarowych
 url_stacje = 'http://api.gios.gov.pl/pjp-api/rest/station/findAll'
@@ -11,6 +12,10 @@ response_stacje = requests.get(url_stacje)
 # Sprawdzenie, czy zapytanie się powiodło
 if response_stacje.status_code == 200:
     lista_stacji = response_stacje.json()
-    print(lista_stacji)
+    df_stacje = pd.json_normalize(lista_stacji)
+
+    print(df_stacje)
+    # print(lista_stacji)
 else:
     print("Błąd podczas pobierania danych stacji")
+
