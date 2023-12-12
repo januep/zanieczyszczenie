544cb1b17d6f1f2dabc8d32d606d2c3c45eaccd4
83691 <83691@student.ue.poznan.pl>
2023-12-12 17:25:32 +0100

    first dev commit

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # Witajcie w moim pierwszym projekcie w Pythonie!
 
-print("hello world!")
+import requests
+
+# URL API do pobrania listy stacji pomiarowych
+url_stacje = 'http://api.gios.gov.pl/pjp-api/rest/station/findAll'
+
+# Wykonanie zapytania do API i przypisanie odpowiedzi do zmiennej
+response_stacje = requests.get(url_stacje)
+
+# Sprawdzenie, czy zapytanie się powiodło
+if response_stacje.status_code == 200:
+    lista_stacji = response_stacje.json()
+    print(lista_stacji)
+else:
+    print("Błąd podczas pobierania danych stacji")
